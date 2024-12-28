e9ed1b26d7dfc321506f4f8431871475fcbe9e85
bkosciow <kosci1@gmail.com>
2024-12-28 22:32:20 +0100

    fix callback

--- display/xy_callback.py
+++ display/xy_callback.py
@@ -1,6 +1,15 @@
 
+# def transform_half(lcd, i, j):
+#     offset_width = lcd.height // 2
+#     if 0 <= i < offset_width:
+#         i = i + offset_width
+#     else:
+#         i = i - offset_width
+#
+#     return (i, j)
+
 def transform_half(lcd, i, j):
-    offset_width = lcd.height // 2
+    offset_width = lcd.width // 2
     if 0 <= i < offset_width:
         i = i + offset_width
     else:
